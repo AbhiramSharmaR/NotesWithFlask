db5da8bf8e198cee6890ea38c45e5641aaf35768
Abhiram <abhiramsharmar@gmail.com>
2025-09-25 22:27:50 +0530

    Version 1

---
+++ website/auth.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return "<h1>login</h1>"
+
+@auth.route('/logout')
+def logout():
+    return "<h1>logout</h1>"
+
+@auth.route('/signUp')
+def signUp():
+    print("Displaying login page")
+    return "<h1>signUp</h1>"
