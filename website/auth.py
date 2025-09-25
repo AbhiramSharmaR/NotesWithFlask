41dbe5406fcbe948d5cbe6d4f013a1090c8c414a
AbhiramSharmaR <abhiramsharmar@gmail.com>
2025-09-25 22:54:33 +0530

    Version 2

--- website/auth.py
+++ website/auth.py
@@ -5,13 +5,13 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<h1>login</h1>"
+    return "<h1>login page</h1>"
 
 @auth.route('/logout')
 def logout():
-    return "<h1>logout</h1>"
+    return "<h1>logout page</h1>"
 
 @auth.route('/signUp')
 def signUp():
     print("Displaying login page")
-    return "<h1>signUp</h1>"
+    return "<h1>signUp page</h1>"
